3306e2cbc26c207c9f0e50139e2993a59acd8c08
macchan.dev <macchan.dev@gmail.com>
2015-04-27 19:24:58 +0900

    OkHttp
    ボタン押下でリクエストする

--- app/src/main/java/com/macchan_dev/librarysample/MainActivity.java
+++ app/src/main/java/com/macchan_dev/librarysample/MainActivity.java
@@ -1,17 +1,31 @@
 package com.macchan_dev.librarysample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
     }
 
 
@@ -35,5 +49,26 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.buttonOkHttp)
+    public void okHttp() {
+        Request request = new Request.Builder()
+                .url("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010")
+                .get()
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String result = response.body().string();
+                Log.d(TAG, result);
+            }
+        });
     }
 }
